245e8e4848fb38b3d3e42e820dac7fe56dd572b6
maeollie <makayla.hipke@gmail.com>
2025-10-20 01:07:32 +0000

    Deploying to gh-pages from @ maeollie/maeollie.github.io@d27fb0af4d8d3b7ad00663ca2019813642a10548 🚀

---
+++ assets/js/search-data.js
@@ -0,0 +1,62 @@
+// get the ninja-keys element
+const ninja = document.querySelector('ninja-keys');
+
+// add the home and posts menu items
+ninja.data = [{
+    id: "nav-about",
+    title: "about",
+    section: "Navigation",
+    handler: () => {
+      window.location.href = "/";
+    },
+  },{id: "nav-my-work",
+          title: "my work",
+          description: "Recent projects and collaborations",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/my-work/";
+          },
+        },{id: "nav-cv",
+          title: "cv",
+          description: "Explore my work history",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/cv/";
+          },
+        },{id: "books-the-godfather",
+          title: 'The Godfather',
+          description: "",
+          section: "Books",handler: () => {
+              window.location.href = "/books/the_godfather.html";
+            },},{id: "projects-project-1",
+          title: 'project 1',
+          description: "with background image",
+          section: "Projects",handler: () => {
+              window.location.href = "/projects/1_project.html";
+            },},{
+      id: 'light-theme',
+      title: 'Change theme to light',
+      description: 'Change the theme of the site to Light',
+      section: 'Theme',
+      handler: () => {
+        setThemeSetting("light");
+      },
+    },
+    {
+      id: 'dark-theme',
+      title: 'Change theme to dark',
+      description: 'Change the theme of the site to Dark',
+      section: 'Theme',
+      handler: () => {
+        setThemeSetting("dark");
+      },
+    },
+    {
+      id: 'system-theme',
+      title: 'Use system default theme',
+      description: 'Change the theme of the site to System Default',
+      section: 'Theme',
+      handler: () => {
+        setThemeSetting("system");
+      },
+    },];
